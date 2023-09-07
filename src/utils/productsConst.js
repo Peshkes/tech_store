b9dc17bf632e18d39882b871da772b155f4fe6d1
JaηMogh <makedonski.dragon@gmail.com>
2023-09-08 00:29:17 +0300

    added more products images

--- src/utils/productsConst.js
+++ src/utils/productsConst.js
@@ -25,6 +25,15 @@ import product3_3_x2 from '../images/products/phones/3/3-3-x2.webp';
 import product3_4 from '../images/products/phones/3/3-4.webp';
 import product3_4_x2 from '../images/products/phones/3/3-4-x2.webp';
 
+import product6_1 from '../images/products/phones/6/6-1.webp';
+import product6_1_x2 from '../images/products/phones/6/6-1-x2.webp';
+import product6_2 from '../images/products/phones/6/6-2.webp';
+import product6_2_x2 from '../images/products/phones/6/6-2-x2.webp';
+import product6_3 from '../images/products/phones/6/6-3.webp';
+import product6_3_x2 from '../images/products/phones/6/6-3-x2.webp';
+import product6_4 from '../images/products/phones/6/6-4.webp';
+import product6_4_x2 from '../images/products/phones/6/6-4-x2.webp';
+
 import product4_1 from '../images/products/laptops/4/4-1.webp';
 import product4_1_x2 from '../images/products/laptops/4/4-1-x2.webp';
 import product4_2 from '../images/products/laptops/4/4-2.webp';
@@ -36,12 +45,41 @@ import product4_4_x2 from '../images/products/laptops/4/4-4-x2.webp';
 
 import product5_1 from '../images/products/laptops/5/5-1.webp';
 import product5_1_x2 from '../images/products/laptops/5/5-1-x2.webp';
-import product5_2 from '../images/products/laptops/4/4-2.webp';
-import product5_2_x2 from '../images/products/laptops/4/4-2-x2.webp';
-import product5_3 from '../images/products/laptops/4/4-3.webp';
-import product5_3_x2 from '../images/products/laptops/4/4-3-x2.webp';
-import product5_4 from '../images/products/laptops/4/4-4.webp';
-import product5_4_x2 from '../images/products/laptops/4/4-4-x2.webp';
+import product5_2 from '../images/products/laptops/5/5-2.webp';
+import product5_2_x2 from '../images/products/laptops/5/5-2-x2.webp';
+import product5_3 from '../images/products/laptops/5/5-3.webp';
+import product5_3_x2 from '../images/products/laptops/5/5-3-x2.webp';
+import product5_4 from '../images/products/laptops/5/5-4.webp';
+import product5_4_x2 from '../images/products/laptops/5/5-4-x2.webp';
+
+import product7_1 from '../images/products/headphones/7/7-1.webp';
+import product7_1_x2 from '../images/products/headphones/7/7-1-x2.webp';
+import product7_2 from '../images/products/headphones/7/7-2.webp';
+import product7_2_x2 from '../images/products/headphones/7/7-2-x2.webp';
+import product7_3 from '../images/products/headphones/7/7-3.webp';
+import product7_3_x2 from '../images/products/headphones/7/7-3-x2.webp';
+import product7_4 from '../images/products/headphones/7/7-4.webp';
+import product7_4_x2 from '../images/products/headphones/7/7-4-x2.webp';
+
+import product8_1 from '../images/products/laptops/8/8-1.webp';
+import product8_1_x2 from '../images/products/laptops/8/8-1-x2.webp';
+import product8_2 from '../images/products/laptops/8/8-2.webp';
+import product8_2_x2 from '../images/products/laptops/8/8-2-x2.webp';
+import product8_3 from '../images/products/laptops/8/8-3.webp';
+import product8_3_x2 from '../images/products/laptops/8/8-3-x2.webp';
+import product8_4 from '../images/products/laptops/8/8-4.webp';
+import product8_4_x2 from '../images/products/laptops/8/8-4-x2.webp';
+
+import product9_1 from '../images/products/laptops/9/9-1.webp';
+import product9_1_x2 from '../images/products/laptops/9/9-1-x2.webp';
+import product9_2 from '../images/products/laptops/9/9-2.webp';
+import product9_2_x2 from '../images/products/laptops/9/9-2-x2.webp';
+import product9_3 from '../images/products/laptops/9/9-3.webp';
+import product9_3_x2 from '../images/products/laptops/9/9-3-x2.webp';
+import product9_4 from '../images/products/laptops/9/9-4.webp';
+import product9_4_x2 from '../images/products/laptops/9/9-4-x2.webp';
+
+
 
 export const productsArr = [
     {
@@ -67,7 +105,7 @@ export const productsArr = [
         name: 'REALME C55 8',
         imgSmall: [product3_1, product3_2, product3_3, product3_4],
         imgBig: [product3_1_x2, product3_2_x2, product3_3_x2, product3_4_x2],
-        rating: 5,
+        rating: 3,
         price: 17990
     },
 
@@ -76,7 +114,52 @@ export const productsArr = [
         name: 'ADATA XPG Xenia 15KC',
         imgSmall: [product4_1, product4_2, product4_3, product4_4],
         imgBig: [product4_1_x2, product4_2_x2, product4_3_x2, product4_4_x2],
+        rating: 4,
+        price: 135990
+    },
+
+    {
+        id: 5,
+        name: 'GMNG Rush',
+        imgSmall: [product5_1, product5_2, product5_3, product5_4],
+        imgBig: [product5_1_x2, product5_2_x2, product5_3_x2, product5_4_x2],
         rating: 5,
         price: 135990
+    },
+
+    {
+        id: 6,
+        name: 'Apple iPhone 15',
+        imgSmall: [product6_1, product6_2, product6_3, product6_4],
+        imgBig: [product6_1_x2, product6_2_x2, product6_3_x2, product6_4_x2],
+        rating: 5,
+        price: 0
+    },
+
+    {
+        id: 7,
+        name: 'Sony WH-1000XM5',
+        imgSmall: [product7_1, product7_2, product7_3, product7_4],
+        imgBig: [product7_1_x2, product7_2_x2, product7_3_x2, product7_4_x2],
+        rating: 5,
+        price: 88390
+    },
+
+    {
+        id: 8,
+        name: 'HP Omen 17-ck0045ur',
+        imgSmall: [product8_1, product8_2, product8_3, product8_4],
+        imgBig: [product8_1_x2, product8_2_x2, product8_3_x2, product8_4_x2],
+        rating: 4,
+        price: 335990
+    },
+
+    {
+        id: 9,
+        name: 'MSI Stealth GS77 12UHS-030RU',
+        imgSmall: [product9_1, product9_2, product9_3, product9_4],
+        imgBig: [product9_1_x2, product9_2_x2, product9_3_x2, product9_4_x2],
+        rating: 4,
+        price: 319990
     }
 ]

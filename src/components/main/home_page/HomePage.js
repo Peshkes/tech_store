bf9d7e9033a00f1ca9a96eb95b5aa583c1e4afab
JaηMogh <makedonski.dragon@gmail.com>
2023-09-08 00:15:58 +0300

    added products images

--- src/components/main/home_page/HomePage.js
+++ src/components/main/home_page/HomePage.js
@@ -2,9 +2,6 @@ import React, {useContext, useEffect} from 'react';
 import style from './homePage.module.css';
 import Slider from "./slider/Slider";
 import {HeaderContext} from "../../../utils/context";
-import ProductsPage from "../products_page/ProductsPage";
-import ProductPage from "../product_page/ProductPage";
-import ProductGallery from "../product_description/product_gallery/ProductGallery";
 import ProductsGallery from "../products_gallery/ProductsGallery";
 
 const HomePage = () => {

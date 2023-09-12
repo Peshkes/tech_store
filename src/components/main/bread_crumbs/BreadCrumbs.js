dd67116e9d91d3a35379757c01320569477dbbb8
JaηMogh <makedonski.dragon@gmail.com>
2023-09-12 23:10:35 +0300

    Changes by peshkes

--- src/components/main/bread_crumbs/BreadCrumbs.js
+++ src/components/main/bread_crumbs/BreadCrumbs.js
@@ -9,7 +9,7 @@ const BreadCrumbs = () => {
     let match = useResolvedPath("").pathname;
     let routeCrumbs = match.split('/');
     let length = routeCrumbs.length;
-    console.log(routeCrumbs);
+
     if(routeCrumbs[length - 2] === 'product')
         routeCrumbs[length - 2] += 's';
 
